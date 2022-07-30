1f2db549475a5110f7f9ec10cb0d7d4b8e7c4e1a
OlgaKiryazova <olgashvets05@gmail.com>
2022-07-30 22:11:33 +0300

    correction mistakes

--- iterator_HM.py
+++ iterator_HM.py
@@ -1,17 +1,9 @@
 # Task 1
 
 def with_index(iterable, start=0):
-    iterator = iter(iterable)
-    next_element_exists = True
-
-    while next_element_exists:
-        try:
-            from_iterator = next(iterator)
-            print(f'{from_iterator} : {start}')
-            start += 1
-        except StopIteration:
-            next_element_exists = False
-
+    for i in iterable:
+        yield i, start
+        start += 1
 
 ##################################################################################
 # Task 2
@@ -20,22 +12,52 @@ def with_index(iterable, start=0):
 def in_range(start, end, step=1):
     if step > 0:
         while start < end:
-            print(start)
+            yield start
             start += step
     elif step < 0:
         while start > end:
-            print(start)
+            yield start
             start += step
-    print('stop')
+
+
+#################################################################################
+# Task 3
+
+class Iterator:
+    def __init__(self, iterable):
+        self.iterable = iterable
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.iterable)-1:
+            self.index += 1
+            return self.iterable[self.index]
+        else:
+            raise StopIteration
+
+    def __getitem__(self, item):
+        return self.iterable[item]
 
 
 
 
 def main():
     a = ['a','b', 'c', 't']
-    with_index(a, 5)
-    in_range(10, 1, -1)
-    in_range(1, 10, 5)
+    for i in with_index(a, 5):
+        print(i)
+    generator = in_range(10, 1, -1)
+    # generator = in_range(1, 10, 2)
+    for i in generator:
+        print(i)
+
+    iterator = Iterator([1, 2, 3, 4, 5])
+    for i in iterator:
+        print(i)
+
+    print(iterator[1])
 
 
 if __name__ == '__main__':
