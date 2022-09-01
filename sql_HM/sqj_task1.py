909d6fe10603678aa0fbd7f6f090c4ad98a9c8a3
OlgaKiryazova <olgashvets05@gmail.com>
2022-09-01 18:38:33 +0300

    Lesson 34. SQL

---
+++ sql_HM/sqj_task1.py
@@ -0,0 +1,139 @@
+from pprint import pprint
+
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(path: str):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+    except Error as e:
+        print(f'Sqlite connectio error {e}')
+
+    return connection
+
+
+def execute_query(connection, query: str):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except Error as e:
+        print(f'Sqlite connectio error {e}')
+
+
+def select_query(connection, query: str):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f'Sqlite connection error {e}')
+
+
+def main():
+    connection = create_connection('task_1.sqlite')
+
+    create_job_title_table = """
+    CREATE TABLE IF NOT EXISTS job_title (
+        id  INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    );
+    """
+
+    create_workers_table = """
+    CREATE TABLE IF NOT EXISTS workers (
+        id  INTEGER PRIMARY KEY AUTOINCREMENT,
+        full_name TEXT NOT NULL,
+        age INTEGER,
+        gender VARCHAR(10),
+        job_title_id INTEGER NOT NULL, 
+        FOREIGN KEY (job_title_id) REFERENCES job_title (id)
+    );
+    """
+
+    create_department_table = """
+    CREATE TABLE IF NOT EXISTS department (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        job_title_id INTEGER NOT NULL, 
+        FOREIGN KEY (job_title_id) REFERENCES job_title (id)
+    )
+    """
+
+    execute_query(connection, create_job_title_table)
+    execute_query(connection, create_workers_table)
+    execute_query(connection, create_department_table)
+
+    create_job_title = """
+    INSERT INTO 
+        job_title (name)
+    VALUES 
+        ('HR manager'),
+        ('recruiter'),
+        ('sales manager'),
+        ('sales'),
+        ('lawyer');
+    """
+
+    create_workers = """
+    INSERT INTO
+        workers (full_name, age, gender,job_title_id)
+    VALUES 
+        ('Ivan Petrov', 30, 'male', 5),
+        ('Oleg Fox', 28, 'male', 4),
+        ('Sveta Kvitka', 20, 'female', 2),
+        ('Victor Chep', 44, 'male', 3),
+        ('Nika Tonhf', 40, 'female', 1);
+    """
+
+    create_department = """
+    INSERT INTO
+        department (title, job_title_id )
+    VALUES 
+        ('Sales department', 3),
+        ('Sales department', 4),
+        ('HR department', 1),
+        ('HR department', 2),
+        ('legal department', 5);
+    """
+
+    # execute_query(connection, create_job_title)
+    # execute_query(connection, create_workers)
+    # execute_query(connection, create_department)
+
+
+    select_departament_workers = """
+    SELECT 
+        workers.id,
+        workers.full_name,
+        workers.age,
+        department.title
+    FROM
+        department
+    INNER JOIN workers on workers.job_title_id = department.job_title_id
+    """
+
+    departament_workers = select_query(connection,  select_departament_workers)
+
+    pprint(departament_workers)
+
+    update_worker = 'UPDATE workers SET age = 35 WHERE full_name = "Victor Chep"'
+    delete_worker = 'DELETE FROM workers WHERE full_name = "Nika Tonhf"'
+
+    execute_query(connection, update_worker)
+    # execute_query(connection, delete_worker)
+
+    print('#'*80)
+    pprint(departament_workers)
+    connection.close()
+
+    # Почему pprint в строчках 122 и 130 выдают одинаковые данные значений из таблиц,
+    #  при этом в строке 127 я меняю данные?
+    #
+
+if __name__ == '__main__':
+    main()
---
+++ sql_HM/sql_task2.py
@@ -0,0 +1,101 @@
+from pprint import pprint
+
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(path: str):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+    except Error as e:
+        print(f'Sqlite connectio error {e}')
+
+    return connection
+
+
+def execute_query(connection, query: str):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+    except Error as e:
+        print(f'Sqlite connectio error {e}')
+
+
+def select_query(connection, query: str):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f'Sqlite connection error {e}')
+
+
+def main():
+    connection = create_connection('hr.db')
+
+# write a query to display the names (first_name, last_name) using alias name
+# "First Name", "Last Name" from the table of employees;
+
+    select_employees = """
+    SELECT 
+        first_name as [First Name], 
+        last_name as [Last Name] 
+     FROM employees
+    """
+
+    full_name_employees = select_query(connection,  select_employees)
+    pprint(full_name_employees)
+
+#write a query to get the unique department ID from the employee table
+    select_department_id = "SELECT DISTINCT department_id FROM employees"
+    unique_dep_id_query = select_query(connection, select_department_id)
+    pprint(unique_dep_id_query)
+
+
+#write a query to get all employee details from the employee table ordered
+# by first name, descending
+    select_all_details_query = "SELECT * FROM employees ORDER BY first_name DESC"
+    get_all_details_query = select_query(connection, select_all_details_query)
+    print(get_all_details_query)
+
+# write a query to get the names (first_name, last_name), salary,
+    # PF of all the employees (PF is calculated as 12% of salary)
+    select_pf_query = """
+    SELECT 
+        first_name, 
+        last_name, 
+        salary, 
+        (salary*0.12)
+    FROM employees
+        """
+    pf_query = select_query(connection, select_pf_query)
+    pprint(pf_query)
+
+#write a query to get the maximum and minimum salary from the employees table
+    select_min_max_salary = "SELECT MIN(salary), MAX(salary) FROM employees"
+    min_max_salary = select_query(connection, select_min_max_salary)
+    pprint(min_max_salary)
+
+#write a query to get a monthly salary (round 2 decimal places) of each
+    # and every employee
+    select_round_salary = """
+    SELECT 
+        first_name, 
+        last_name, 
+        ROUND(salary-salary*0.12, 2)
+    FROM employees 
+    """
+    round_salary = select_query(connection, select_round_salary)
+    pprint(round_salary)
+
+
+
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
